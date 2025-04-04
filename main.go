d983e7049b645347489549c549a05e1c6ffcb89d
mickk <mikerloper@gmail.com>
2025-04-04 16:58:39 -0700

    added commands map and register handler.
    also added migrations and sqlc integration.

--- main.go
+++ main.go
@@ -1,22 +1,136 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 	"github.com/mickk/gatorcli/internal/config"
+	"github.com/mickk/gatorcli/internal/database"
 )
+
+type state struct {
+	db     *database.Queries
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	list map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.list[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	f, exists := c.list[cmd.name]
+	if !exists {
+		return errors.New("command does not exist")
+	}
+
+	return f(s, cmd)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("username arg is required")
+	}
+
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	err = s.config.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("user has been set")
+
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("name arg is required")
+	}
+
+	regParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+
+	user, err := s.db.CreateUser(context.Background(), regParams)
+	if err != nil {
+		return fmt.Errorf("unable to create user: %v", err)
+	}
+
+	fmt.Printf("user %v has been created\n", user.Name)
+
+	err = s.config.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("user has been set")
+
+	return nil
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Printf("unable to read config file: %v\n", err)
+		os.Exit(1)
 	}
 
-	cfg.SetUser("mike")
-	cfg, err = config.Read()
+	s := state{
+		config: &cfg,
+	}
+
+	db, err := sql.Open("postgres", cfg.DbURL)
 	if err != nil {
-		fmt.Printf("unable to read config file: %v\n", err)
+		fmt.Printf("cannot connect to db: %v\n", err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("Current Config: %+v\n", cfg)
+	s.db = database.New(db)
+
+	cmds := commands{
+		make(map[string]func(*state, command) error),
+	}
+
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("command is required")
+		os.Exit(1)
+	}
+
+	cmd := command{
+		name: args[1],
+		args: args[2:],
+	}
+
+	err = cmds.run(&s, cmd)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
 }
